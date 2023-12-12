675956119168eb0486767a05051fcc66ae5bd8cf
Vincent <j.vinc-sites@yahoo.com>
2023-12-12 23:04:10 +0100

    selection du resultat dans main

--- src/main.py
+++ src/main.py
@@ -15,12 +15,12 @@ def main():
                   Reg(Defer(REG_BITS, lambda: r3)),
                   Reg(Defer(REG_BITS, lambda: r4)),
                   Reg(Defer(REG_BITS, lambda: r5)) )
-	
+
     flags_old = ( Reg(Defer(REG_BITS, lambda: flag_z)),
                   Reg(Defer(REG_BITS, lambda: flag_n)),
                   Reg(Defer(REG_BITS, lambda: flag_c)),
                   Reg(Defer(REG_BITS, lambda: flag_v)) )
-	 
+ 
 
     instruction = ROM(REG_BITS, WORD_SIZE, pc_old)
     opcode = instruction[0 : OPCODE_BITS]
@@ -30,19 +30,23 @@ def main():
 
     alu_set = alu(instruction, regs_old)                     # renvoie (regs, flags)
     shift_set = shift(instruction, regs_old)                 # renvoie (regs) sous forme de 1-uplet
-    load_store_set = load_store(instruction, regs_old)             # renvoie (regs)
+    load_store_set = load_store(instruction, regs_old)       # renvoie (regs)
     jmp_set = jmp(instruction, regs_old, flags_old, pc_old)  # renvoie (regs, pc)
 
     # selection du resultat grace a l'opcode et les resultats recus
 
-    r2 = regs_old[0]
-    r3 = regs_old[1]
-    r4 = regs_old[2]
-    r5 = regs_old[3]
-    flag_z = flags_old[0]
-    flag_n = flags_old[1]
-    flag_c = flags_old[2]
-    flag_v = flags_old[3]
-    pc = incr(pc_old)
+    pc_if_incr = incr(pc_old)
+    pc = mux_jmp(opcode, pc_if_incr, jmp_set[1])
 
-    instruction.set_as_output("instruction")
+    flags = mux_alu(opcode, flags_old, alu_set[1])
+    flag_z = flags[0]
+    flag_n = flags[1]
+    flag_c = flags[2]
+    flag_v = flags[3]
+
+    regs = select_opcode(opcode, alu_set[0], shift_set[0], load_store_set[0], jmp_set[0])
+    r2 = regs[0]
+    r3 = regs[1]
+    r4 = regs[2]
+    r5 = regs[3]
+
--- src/tools.py
+++ src/tools.py
@@ -1,5 +1,17 @@
 from constants import *
 
 def incr(a):
-	# TODO
-	return a
+    # TODO
+    return a
+
+def mux_jmp(opcode, not_jmp, is_jmp):
+    # TODO
+    return not_jmp
+
+def mux_alu(opcode, not_alu, is_alu):
+    # TODO
+    return not_alu
+
+def select_opcode(opcode, alu, shift, load_store, jmp):
+    # TODO
+    return alu
